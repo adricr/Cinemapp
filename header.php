9b01837e3658d843c9f72fe3aed3fa653ff6e333
Adrian <adriancorderorodriguez@gmail.com>
2023-11-17 11:12:50 +0000

    header simplified and fixed to represent the current functionality of the page

--- header.php
+++ header.php
@@ -17,20 +17,13 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="index.php" class="nav-link px-2 text-light">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-light">Movies</a></li>
-          <li><a href="#" class="nav-link px-2 text-light">Account</a></li>
-          <li><a href="#" class="nav-link px-2 text-light">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-light">About</a></li>
+          <li><a href="#" class="nav-link px-2 text-light">WHAT'S ON</a></li>
+          <li><a href="#" class="nav-link px-2 text-light">ACCOUNT</a></li>
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
 
         <div class="text-end"> 
-          <a class="btn btn-outline-light me-2" href="sign_up.php">Sign-up</a>
-          <button type="button" class="btn btn-warning">Log in</button>
+          <button type="button" class="btn btn-warning">Log out</button>
         </div>
       </div>
 
