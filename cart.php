dd872048b656aa533141fa5532c913dde564dc07
Adrian <adriancorderorodriguez@gmail.com>
2023-12-06 14:51:56 +0000

    modified to tell if the cart is empty or not

--- cart.php
+++ cart.php
@@ -16,7 +16,7 @@ require('db_connection.php');
 
 </head>
 
-<body>
+<body class ="bg-dark">
   <header class="p-1 bg-dark text-white">  
       <div class="container">
       <?php include "header.php";?>
@@ -25,6 +25,7 @@ require('db_connection.php');
     <main>
 
         <?php
+        if(isset($_SESSION['movie'])){
         echo'
         <div class="container-fluid bg-dark text-white text-center" >
             <h1 class="display-1" >Shopping Cart</h1>
@@ -46,16 +47,33 @@ require('db_connection.php');
                         <td class="fw-bold text-warning">'.$_SESSION['movie']['tickets']*$_SESSION['movie']['price'].' £</td>
                         </tr>
                         </tbody>
-                        </table> 
+                        </table>
+                        <form action="cart_tools.php" method= "post">
+                        <input type="hidden" name="user_id" value="'.$_SESSION['user_id'].'" >
+                        <input type="hidden" name="movie_id" value="'.$_SESSION['movie']['id'].'" >
+                        <input type="hidden" name="tickets" value="'.$_SESSION['movie']['tickets'].'" >   
+                        <button class= "btn btn-warning" type="submit">Book Now!</button>                  
+                        </form> 
                     </div>    
                 </div>
         </div>
-        ';
+        ';}
+        else{
+          echo'
+          <div class="container-fluid bg-dark text-white text-center" >
+              <h1 class="display-1" >Shopping Cart</h1>
+                  <div class="row">
+                      <h2 class = "display-2 mt-5 pt-5">Your cart is empty</h2>  
+                  </div>
+          </div>
+          '; 
+        }
         ?>
 
     </main>
   <footer>
-    <!-- place footer here -->
+  
+  <!-- place footer here -->
   </footer>
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
